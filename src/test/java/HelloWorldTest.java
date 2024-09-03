0783c497011e065f32fe66fb7d5af789b23ec0e1
YriySmirnov <ani-virus@ya.ru>
2024-09-03 09:47:05 +0300

    HelloWorldTest redirect Location

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,19 +1,19 @@
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.Map;
 
 public class HelloWorldTest {
 
     @Test
     public void testRestAssured() {
-        JsonPath response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_json_homework")
-                .jsonPath();
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
 
-        ArrayList<Map<String, String>> messages = response.get("messages");
-        System.out.println(messages.get(1));
+        String Location = response.getHeader("Location");
+        System.out.println(Location);
     }
 }

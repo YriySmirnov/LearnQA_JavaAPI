e63b2fbd0d44bb53491b46d6a78e5433d0192cf9
YriySmirnov <ani-virus@ya.ru>
2024-09-03 09:36:21 +0300

    HelloWorldTest json message

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,11 +1,19 @@
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class HelloWorldTest {
+
     @Test
-    public void testHelloWorld() {
-        Response response = RestAssured.get("https://playground.learnqa.ru/api/hello").andReturn();
-        response.prettyPrint();
+    public void testRestAssured() {
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        ArrayList<Map<String, String>> messages = response.get("messages");
+        System.out.println(messages.get(1));
     }
 }

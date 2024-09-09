9bbbf943ac4685dbed3301aa02a592e6de4f7f6b
YriySmirnov <ani-virus@ya.ru>
2024-09-09 08:56:54 +0300

    HelloWorldTest assert Cookie

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,13 +1,25 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.*;
 
 public class HelloWorldTest {
 
     @Test
     public void testRestAssured() {
-        String string = "вкрыфкрыкрыкрырырыу";
+        Map<String, String> userAuth = new HashMap<>();
+        userAuth.put("email", "vinkotov@example.com");
+        userAuth.put("passwod", "1234");
 
-        Assertions.assertTrue(string.length()>=15, "Строка не длиннее 15 символов");
+        Response response = RestAssured
+                    .given()
+                    .body(userAuth)
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
 
+        Map<String, String> responseCookie = response.getCookies();
+        Assertions.assertEquals("hw_value", responseCookie.get("HomeWork"), "Cookie not correct");
     }
 }

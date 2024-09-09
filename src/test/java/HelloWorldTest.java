2de9c2f61ff694b48e4e2e7bee023bee354f9973
YriySmirnov <ani-virus@ya.ru>
2024-09-09 11:37:58 +0300

    HelloWorldTest ParameterizedTest

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,26 +1,38 @@
 import io.restassured.RestAssured;
-import io.restassured.http.Headers;
-import io.restassured.response.Response;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class HelloWorldTest {
 
-    @Test
-    public void testRestAssured() {
+    @ParameterizedTest
+    @ValueSource(strings={"platform", "browser", "device"})
+    public void testRestAssured(String param) {
+
         Map<String, String> userAuth = new HashMap<>();
         userAuth.put("email", "vinkotov@example.com");
         userAuth.put("passwod", "1234");
 
-        Response response = RestAssured
+        JsonPath response = RestAssured
                     .given()
                     .body(userAuth)
-                .get("https://playground.learnqa.ru/api/homework_header")
-                .andReturn();
-        Headers responseHeader = response.getHeaders();
-        Assertions.assertEquals("Some secret value", responseHeader.getValue("x-secret-homework-header"), "Header not have homework");
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .jsonPath();
+
+        String expectedParam = response.get(param);
+
+        Map<String, String> responseParam = new HashMap<>();
+        responseParam.put(param, expectedParam);
+        response = RestAssured
+                .given()
+                .body(responseParam)
+                .post("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .jsonPath();
+        String actualParam = response.get(param);
+
+        Assertions.assertEquals(expectedParam, actualParam, "Parameters match");
     }
 }
